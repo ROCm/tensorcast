b65bca581e706a2776d2913fc381e6924a75c055
Alireza Khodamoradi <alirezak@xilinx.com>
2024-03-15 15:53:50 -0600

    workaround for fnuz+torch_float8

--- tests/utils.py
+++ tests/utils.py
@@ -5,6 +5,4 @@ def compare_2(tensor1, tensor2):
         if torch.allclose(tensor1, tensor2):
             return
         else:
-            print("tensor1: ", tensor1)
-            print("tensor2: ", tensor2)
             raise ValueError
