2d50841b7fda923dead28ca5bfcb7d716765bf75
Eric Dellinger <ericd@amd.com>
2025-03-18 16:30:50 -0600

    ensure virtual mode only; increased tests

--- tests/base_tests_v2.py
+++ tests/base_tests_v2.py
@@ -17,7 +17,7 @@ logger = tcast.get_logger("tcast_unittest")
 
 
 def test_torchcast_virtual_unscaled_base(
-    dtype: tcast.DataType, torch_dtype: torch.dtype, shape: tuple[int], roundmode: str, computemode: str, castmode: str
+    dtype: tcast.DataType, torch_dtype: torch.dtype, shape: tuple[int], roundmode: str, computemode: str
 ):
     """Test torchcast with virtual mode and unscaled data."""
     print(dtype, isinstance(dtype, str), isinstance(dtype, tcast.DataType))
@@ -25,25 +25,21 @@ def test_torchcast_virtual_unscaled_base(
         dtype = tcast.DataType(dtype)
     print(dtype, isinstance(dtype, str), isinstance(dtype, tcast.DataType))
     tcastv2.initialize(roundmode=roundmode, compmode=computemode)
-    tcast.initialize(roundmode=roundmode, computemode=computemode, castmode=castmode)
+    tcast.initialize(roundmode=roundmode, computemode=computemode, castmode="virtual")
     tensor = torch.randn(shape, device="cuda", dtype=torch_dtype)
     dtype_v2 = convert_triton_dtype_to_v2(dtype)
     assert isinstance(dtype_v2, tcastv2.DataType)
     tensor_v2 = tcastv2.cast(tensor, dtype_v2).tensor
     tensor_tc = tcast.cast(tensor, dtype)
-    if tcast.Modes.cast != tcast.CastMode.VIRTUAL:
-        tensor_tc = tensor_tc.output
     torch.testing.assert_close(tensor_v2, tensor_tc)
 
 
 def test_torchcast_virtual_unscaled_torch_base(
-    dtype: tcast.DataType, torch_dtype: torch.dtype, shape: tuple[int], roundmode: str, computemode: str, castmode: str
+    dtype: tcast.DataType, torch_dtype: torch.dtype, shape: tuple[int], roundmode: str, computemode: str
 ):
     """Test torchcast with virtual mode and tcast dtypes that have a torch dtype, compare to torch ".to"."""
-    tcast.initialize(roundmode=roundmode, computemode=computemode, castmode=castmode)
+    tcast.initialize(roundmode=roundmode, computemode=computemode, castmode="virtual")
     tensor = torch.randn(shape, device="cuda", dtype=torch_dtype)
-    tensor_pt = tensor.to(dtype.nspec.torch_dtype)
+    tensor_pt = tensor.to(dtype.nspec.torch_dtype).to(tensor.dtype)
     tensor_tc = tcast.cast(tensor, dtype)
-    if tcast.Modes.cast != tcast.CastMode.VIRTUAL:
-        tensor_tc = tensor_tc.output
     torch.testing.assert_close(tensor_pt, tensor_tc)
--- tests/test_torch_virtual.py
+++ tests/test_torch_virtual.py
@@ -9,32 +9,30 @@ import torch
 
 import tcast
 import tests.base_tests_v2 as B
-from tests.utils import SHAPES_2D, TORCH_DTYPES_16, TORCH_DTYPES_32_16
+import tests.utils as U
 
 
 @pytest.mark.parametrize("dtype", tcast.DataType.gather_registered(lambda x: x.is_unscaled and x.nspec.bits <= 16))
-@pytest.mark.parametrize("torch_dtype", TORCH_DTYPES_32_16)
-@pytest.mark.parametrize("shape", SHAPES_2D)
+@pytest.mark.parametrize("torch_dtype", U.TORCH_DTYPES_32_16)
+@pytest.mark.parametrize("shape", U.SHAPES_234D)
 @pytest.mark.parametrize("roundmode", ["even"])
 @pytest.mark.parametrize("computemode", ["torch"])
-@pytest.mark.parametrize("castmode", ["virtual"])
 def test_torchcast_virtual_unscaled(
-    dtype: tcast.DataType, torch_dtype: torch.dtype, shape: tuple[int], roundmode: str, computemode: str, castmode: str
+    dtype: tcast.DataType, torch_dtype: torch.dtype, shape: tuple[int], roundmode: str, computemode: str
 ):
     """Test torchcast with virtual mode and unscaled data."""
-    B.test_torchcast_virtual_unscaled_base(dtype, torch_dtype, shape, roundmode, computemode, castmode)
+    B.test_torchcast_virtual_unscaled_base(dtype, torch_dtype, shape, roundmode, computemode)
 
 
 @pytest.mark.parametrize("dtype", tcast.DataType.gather_registered(lambda x: x.nspec.torch_dtype is not None))
-@pytest.mark.parametrize("torch_dtype", TORCH_DTYPES_16)
-@pytest.mark.parametrize("shape", SHAPES_2D)
+@pytest.mark.parametrize("torch_dtype", U.TORCH_DTYPES_32_16)
+@pytest.mark.parametrize("shape", U.SHAPES_234D)
 @pytest.mark.parametrize("roundmode", ["even"])
 @pytest.mark.parametrize("computemode", ["torch"])
-@pytest.mark.parametrize("castmode", ["virtual"])
 def test_torchcast_virtual_unscaled_torch(
-    dtype: tcast.DataType, torch_dtype: torch.dtype, shape: tuple[int], roundmode: str, computemode: str, castmode: str
+    dtype: tcast.DataType, torch_dtype: torch.dtype, shape: tuple[int], roundmode: str, computemode: str
 ):
     """Test torchcast with virtual mode and tcast dtypes that have a torch dtype, compare to torch ".to"."""
     if dtype.nspec.torch_dtype == torch_dtype:
         pytest.skip("tcast dtype is the same as torch_dtype")
-    B.test_torchcast_virtual_unscaled_torch_base(dtype, torch_dtype, shape, roundmode, computemode, castmode)
+    B.test_torchcast_virtual_unscaled_torch_base(dtype, torch_dtype, shape, roundmode, computemode)
